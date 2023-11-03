3c1231dea0186853c5c9111f13496fd5c0bab2ec
TaTuanHuy <89507792+TaTuanHuy@users.noreply.github.com>
2023-11-03 10:34:26 +0700

    update edit order

--- src/routes/order.js
+++ src/routes/order.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const OrderProduct = require("../app/controlers/OrderController");
 const { authMiddleWare, authUserMiddleWare } = require("../app/Middleware/authMiddleware");
 
-router.patch("/edit-order/:id", OrderProduct.editOrder);
+router.patch("/deliver-order/:id", OrderProduct.isDeliverOrder);
+router.patch("/edit-order/:id", OrderProduct.editOrder)
 router.post("/create-order", OrderProduct.createOrder);
 router.get("/get-order", OrderProduct.getAllOrder);
 router.post("/find-by-range", OrderProduct.findByRange)
